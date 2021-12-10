4da86ccf2d72bbc9715993fe5b9861aea691f0d9
alexis-cmps <95345100+alexis-cmps@users.noreply.github.com>
2021-12-10 15:23:34 +0100

    caseAlex

--- Case.java
+++ Case.java
@@ -1,58 +1,61 @@
-public class Case{
+//package scrabbleSAE;
+
+public class Case {
+    
+    private static int[] nbPointsJeton={1,3,3,2,1,4,2,4,1,8,10,1,2,1,1,3,8,1,1,1,1,4,10,10,10,10};
     private int couleur;
-    private boolean statut;
+    private boolean caseStatue; // true =recouverte  false = libre
     private char lettre;
 
-    /**
-     * Constructeur de base
-     * @param uneCouleur
-     */
-    public Case(int uneCouleur){
-        this.couleur = uneCouleur;
-        this.statut = statut;
-    }
+
+    //-------methode---------
 
     /**
-     * Getter Couleur
-     * @return
-     */
-    public int getCouleur(){
+    * pré-requis : uneCouleur est un entier entre 1 et 5
+    * action : constructeur de Case
+    */
+    public Case(int uneCouleur){
+        if(uneCouleur>=1 && uneCouleur<=5){
+            this.couleur= uneCouleur;
+        }
+    }
+    /**
+    * résultat : la couleur de this, un nombre entre 1 et 5
+    */
+    public int getCouleur (){
         return this.couleur;
     }
     /**
-     * Getter Lettre
-     * @return
-     */
-    public char getLettre(){
-        return this.lettre;
-    }
-    /**
-     * Setter Lettre
-     * @param let
-     */
-    public void setLettre(char let){
-        let = this.lettre;
-    }
-    
-    /**
-     * Retourne true si la case est recouverte par une lettre 
-     * @return
-     */
-    public boolean estRecouverte(){
-        res = false;
-        if (statut == true){
-            res = true;
+    * pré-requis : cette case est recouverte
+    */
+    public char getLettre (){
+        if(caseStatue==true){ 
+            return this.lettre;
         }
-        return res;
     }
 
     /**
-     *  A REVOIR
-     * toString
-     */
-    public String toString(){
-        return lettre;
+    * pré-requis : let est une lettre majuscule
+    */
+    public void setLettre (char let){
+        if(Character.isUpperCase(let)==true){
+            this.lettre=let;
+        }
+    }
+    /**
+    * résultat : vrai ssi la case est recouverte par une lettre
+    */
+    public boolean estRecouverte (){
+        boolean recouverte =false;
+        if(this.caseStatue==true){
+            recouverte=true;
+        }
+        return recouverte;
     }
 
+    public String toString(){
+        return "couleur : "+couleur+", "+"statue : "+caseStatue+", " + "lettre : " +lettre;
+    }
 }
+
 
